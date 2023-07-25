7f2f619762c4187ad315bc5b255c0271c62c2052
higortorquetti <higor.torquetti@gmail.com>
2023-07-25 15:34:28 -0300

    adição de mais um test e acesso a outro end-pint

--- src/test/Java/SignTest.java
+++ src/test/Java/SignTest.java
@@ -9,20 +9,36 @@ import java.util.concurrent.TimeUnit;
 public class SignTest {
     @Test
     public void testFazerLoginNoTaskit(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Estudos QA\\Selenium\\chromedriver_win32\\chromedriver.exe");
+        //Primeiro configuramos o caminho onde esta o webdriver e depois instanciasmos ele na variável navegator
+        System.setProperty("webdriver.chrome.driver", "caminhoOndeEstaOexeDoWebdriver");
         WebDriver navegator = new ChromeDriver();
         navegator.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
+        //Agora acessamos o end point do teste
         navegator.get("http://www.juliodelima.com.br/taskit");
 
+        //antes de começar maximizo a tela pra ficar em tela cheia
         navegator.manage().window().maximize();
+
+        //Agora clico no botão sign para efetuar o login
         navegator.findElement(By.linkText("Sign in")).click();
-        navegator.findElement(By.id("signinbox")).findElement(By.name("login")).sendKeys("");
-        navegator.findElement(By.id("signinbox")).findElement(By.name("password")).sendKeys("");
+        // aqui passo os dados do login para test
+        navegator.findElement(By.id("signinbox")).findElement(By.name("login")).sendKeys("seusdados");
+        navegator.findElement(By.id("signinbox")).findElement(By.name("password")).sendKeys("suasenha");
+        // por fim clico no botão para efetuar o login
         navegator.findElement(By.linkText("SIGN IN")).click();
+        // após o login feito crio um variável para verificar se deu tudo certo com o login
         String saudacao = navegator.findElement(By.className("me")).getText();
+        // através dessa váriavel faço o test se o valor esperado corresponde com o valor apresentado
         assertEquals("Hi, higor", saudacao);
 
-       navegator.quit();
+        // após confirmar o login clico no botão para ir para a tela de criação de novas tarefas
+        navegator.findElement(By.className("red")).click();
+
+        // aqui faço mais um uma verificação pra ter certeza que consegui acessar o end point correto semelhante a anterior
+        String telaTask = navegator.findElement(By.linkText("My tasks")).getText();
+        assertEquals("My tasks", telaTask);
+
+        navegator.quit();
     }
 }
